a1eb5f2fd22f6d942c09480290bab4a64abf0653
SMukalla <sandhya.mukalla@gmail.com>
2025-06-10 23:04:03 +0530

    Table of 9

---
+++ src/TablePrint.java
@@ -0,0 +1,19 @@
+public class TablePrint {
+
+    public static void main(String[] args) {
+        int a=9;
+        
+
+        System.out.printf("Multiplication Table for 9\n");
+        System.out.printf("%d x %d = %d\n",a,1,a*1);
+        System.out.printf("%d x %d = %d\n",a,2,a*2);
+        System.out.printf("%d x %d = %d\n",a,3,a*3);
+        System.out.printf("%d x %d = %d\n",a,4,a*4);
+        System.out.printf("%d x %d = %d\n",a,5,a*5);
+        System.out.printf("%d x %d = %d\n",a,6,a*6);
+        System.out.printf("%d x %d = %d\n",a,7,a*7);
+        System.out.printf("%d x %d = %d\n",a,8,a*8);
+        System.out.printf("%d x %d = %d\n",a,9,a*9);
+        System.out.printf("%d x %d = %d\n",a,10,a*10);
+    }
+}
